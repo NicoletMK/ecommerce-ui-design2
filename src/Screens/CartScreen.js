184ffdb558214447e0e0abf32d63d093352edb3d
Musquan <musquanmomin@gmail.com>
2023-11-12 23:58:08 -0600

    Cart Screen Updated

--- src/Screens/CartScreen.js
+++ src/Screens/CartScreen.js
@@ -1,40 +1,40 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import NavBar from '../Components/NavigationBar';
 import './ScreenCss/CartScreen.css';
-import Sony1 from '../Images/Sony1.jpeg'
-import SonyV from '../Images/SonyVloggerKit1.jpeg'
-import Sharper from '../Images/SharperMassager1.jpeg'
+import Sony1 from '../Images/Sony1.jpeg';
+import SonyV from '../Images/SonyVloggerKit1.jpeg';
+import Sharper from '../Images/SharperMassager1.jpeg';
 
 function CartScreen() {
-  // Mock data for testing UI
+
+  const navigate = useNavigate();
+
   const [products, setProducts] = useState([
     {
       productId: 1110,
       productName: 'Sony Camera',
       productImage: Sony1,
       quantity: 1,
-      price: 499.99, // Add product price
+      price: 499.99,
     },
     {
       productId: 1112,
       productName: 'Sony Vlogger Kit',
       productImage: SonyV,
       quantity: 1,
-      price: 59.99, // Add product price
+      price: 59.99,
     },
     {
       productId: 3399,
-      productName: 'Shaper Massager',
+      productName: 'Sharper Massager',
       productImage: Sharper,
       quantity: 1,
-      price: 99.99, // Add product price
+      price: 99.99,
     },
-    // Add more mock products as needed
   ]);
 
   const handleRemoveFrom = (productId) => {
-    // Add logic to handle removal (if needed)
     setProducts((prevProducts) => prevProducts.filter((product) => product.productId !== productId));
   };
 
@@ -56,51 +56,43 @@ function CartScreen() {
     );
   };
 
-  // Calculate total price
-  const totalPrice = products.reduce((total, product) => total + product.quantity * product.price, 0);
-
-  // Checkout function (you can replace this with your actual checkout logic)
-  const handleCheckout = () => {
-    console.log('Processing checkout...');
-    // Add your checkout logic here
-  };
-
   return (
     <div className="MainContainerForProductCartScreen">
       <NavBar />
-    
+
       <div className="ProductsAndOrderSummaryContainer">
-      
         <div className="CartBox">
-        <h2>Shopping Cart</h2>
+          <h2>Shopping Cart</h2>
           <div className="ProductsInCartContainer">
             {products.map((product, index) => (
               <div key={index} className="ProductCard">
                 <img src={product.productImage} alt="Product" />
                 <div className="ProductsDetails">
-                <ul>
-                  <li> Product ID: {product.productId} </li>
-                  <li> Product Name: {product.productName} </li>
-                  <li>
-                    Qty: <button onClick={() => handleDecreaseQuantity(product.productId)}>-</button>{' '}
-                    {product.quantity} <button onClick={() => handleIncreaseQuantity(product.productId)}>+</button>
-                  </li>
-                  <div>
-                    <button className="RemoveItemButton" onClick={() => handleRemoveFrom(product.productId)}>
-                      Remove Item
-                    </button>
-                  </div>
-                </ul>
+                  <ul>
+                    <li> Product ID: {product.productId} </li>
+                    <li> Product Name: {product.productName} </li>
+                    <li>
+                      Qty: <button onClick={() => handleDecreaseQuantity(product.productId)}>-</button>{' '}
+                      {product.quantity} <button onClick={() => handleIncreaseQuantity(product.productId)}>+</button>
+                    </li>
+                    <li> Unit Price: ${product.price.toFixed(2)} </li>
+                    <li> Total Price: ${(product.price * product.quantity).toFixed(2)}</li>
+                    <div>
+                      <button className="RemoveItemButton" onClick={() => handleRemoveFrom(product.productId)}>
+                        Remove Item
+                      </button>
+                    </div>
+                  </ul>
+                </div>
               </div>
-            </div>
             ))}
           </div>
         </div>
 
         <div className="OrderSummaryContainer">
           <h3>Order Summary</h3>
-          <p>Total Price: ${totalPrice.toFixed(2)}</p>
-          <button onClick={handleCheckout} className="CheckoutButton">
+          <p>Total Price: ${products.reduce((total, product) => total + product.price * product.quantity, 0).toFixed(2)}</p>
+          <button className="CheckoutButton" onClick={() => navigate('/CheckoutScreen')}>
             Checkout
           </button>
         </div>
@@ -108,6 +100,5 @@ function CartScreen() {
     </div>
   );
 }
-
 
 export default CartScreen;
