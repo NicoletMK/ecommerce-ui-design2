d2af9d78b98c59dd8467ef1a2df180d5f76cc65c
ShresthaSamita <samitashresthasam@gmail.com>
2023-11-18 16:08:35 -0600

    Link to the social media in footer is updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './ComponentCss/Footer.css';
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
@@ -9,20 +8,20 @@ function Footer() {
         <footer className='footerAllContainer'>
             <div className="footerContent">
                 <div className="footerSection">
-                <h3 className="footer-Title"><Link to='/MainScreen' className="navbar-brand">TechnoTreasure</Link></h3>
+                    <h3 className="footer-Title"><a href='/MainScreen' className="navbar-brand">TechnoTreasure</a></h3>
                     <p className="footerDescription">TechnoTreasure is your go-to destination for all things tech.</p>
                     <div className="socialMedia">
-                        <Link to="/social/facebook" className="socialLink"><FontAwesomeIcon icon={faFacebook} /></Link>
-                        <Link to="/social/twitter" className="socialLink"><FontAwesomeIcon icon={faTwitter} /></Link>
-                        <Link to="/social/instagram" className="socialLink"><FontAwesomeIcon icon={faInstagram} /></Link>
+                        <a href="https://www.facebook.com/TechnoTreasureUI" className="socialLink" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} /></a>
+                        <a href="https://twitter.com/TechnoTreUI" className="socialLink" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
+                        <a href="https://www.instagram.com/technotreasure_/" className="socialLink" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
                     </div>
                 </div>
                 <div className="footerSection links">
                     <h3 className="footerTitle">Quick Links</h3>
                     <ul className='footerList'>
-                        <li><Link to='/ContactUsScreen' className='footerLink'>Contact Us</Link></li>
-                        <li><Link to='/FAQsScreen' className='footerLink'>FAQs</Link></li>
-                        <li><Link to='/PrivacyPolicyScreen' className='footerLink'>Privacy Policy</Link></li>
+                        <li><a href='/ContactUsScreen' className='footerLink'>Contact Us</a></li>
+                        <li><a href='/FAQsScreen' className='footerLink'>FAQs</a></li>
+                        <li><a href='/PrivacyPolicyScreen' className='footerLink'>Privacy Policy</a></li>
                     </ul>
                 </div>
                 <div className="footerBottom">
